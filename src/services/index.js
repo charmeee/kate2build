d6a6e541d8ce7ba367424566d9490304bbb495de
charmeee <mandugohigh@gmail.com>
2023-08-03 17:45:04 +0900

    fix: baseurl fix

--- src/services/index.js
+++ src/services/index.js
@@ -2,8 +2,10 @@ import axios from "axios";
 import cookie from 'react-cookies';
 import {ErrorType} from "./type";
 
+const staticServerUri = process.env.REACT_APP_PATH || process.env.REACT_APP_API_URL;
+
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: staticServerUri,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
